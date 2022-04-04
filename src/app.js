ff53d09a2392eb7265bdd513e1f814353f8ce041
Wesley Tavares <w.souzatavares@gmail.com>
2022-04-03 21:16:29 -0300

    feat: adiciona rota para a calculadora de tempo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const timeCalculator = require("./domain/TimeCalculator");
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({
     message: "Hello Universe!"
   });
+});
+
+app.get("/time-calculator/calculate/:start/:end", (req, res) => {
+  const { start, end } = req.params;
+  const result = timeCalculator.calculate(start, end);
+  res.json(result);
 });
 
 module.exports = app;
--- src/app.test.js
+++ src/app.test.js
@@ -9,3 +9,17 @@ describe("Root Endpoint", () => {
     expect(res.body).toEqual({ message: "Hello Universe!" });
   });
 });
+
+describe("Time Calculator Endpoint", () => {
+  test(`De 19:03 às 06:59 retorna 296min diurnos e 420min noturnos`, async () => {
+    const startEncoded = encodeURI("19:03");
+    const endEncoded = encodeURI("06:59");
+    const res = await request(app)
+      .get(`/time-calculator/calculate/${startEncoded}/${endEncoded}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      daytimeInMinutes: 296,
+      nocturnalInMinutes: 420
+    });
+  });
+});
